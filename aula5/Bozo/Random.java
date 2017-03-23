6b005f7a1787de5dddf93155a93b934a3befd5c4
felsiq <felipe.siqueira@usp.br>
2017-03-23 09:49:38 -0300

    Trab

--- aula5/Bozo/Random.java
+++ aula5/Bozo/Random.java
@@ -1,3 +1,24 @@
 class Random{
+	long seed;
+
+	public int get(){
+		seed = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1);
+		return (int) (seed >>> (16));
+	}
 	
+	public int get(int maxVal){
+		return Math.abs((get() % (maxVal + 1)));
+	}
+
+	public int get(int minVal, int maxVal){
+		return get(maxVal - minVal) + minVal;
+	}
+
+	Random(long newSeed){
+		seed = newSeed;
+	}
+
+	Random(){
+		seed = 123;
+	}
 }
